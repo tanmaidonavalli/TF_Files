ac3ce8c626ee4ba7f6dcc80643c8c685fea796b5
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-04 12:27:12 -0700

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -euo pipefail
+
+git clone https://github.com/tanmaidonavalli/TF_Files/DockerFile
+
+#Build Image
+$docker build -t tfimage:latest .
+
+#Create Container
+$docker run -it -d --name tfcontainer tfimage:latest /bin/bash
+
+#login to container
+$docker exec -it tfcontainer /bin/bash
+
+
+$terraform --version
+
+aws configure -u $aws_access_key_id -p $aws_secret_access_key --output none
+
+#Intialize the folder
+$terraform init -no-color -upgrade=true -backend-config="storage_account_name=$S3Bucket_name"
+
+#Run config-lint
+$config-lint -terraform TF_Files
+
+$terraform plan -no-color -out=tfplan | tee plan.output 
+
+if [[ "${FORCE:-false}" == "true" ]];
+   
+    then
+        
+      $terraform apply -no-color tfplan
+
+        else
+
+        echo "##vso[task.logissue type=error]Please check the terraform plan and approve destruction of resource(s) by setting release variable FORCE to true!"
+        exit 1
+fi
+
+
+
+
