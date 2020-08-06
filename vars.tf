664da51acd2dadf837af91ed4cb776cfdadae539
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-05 20:48:30 -0700

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,7 +1,7 @@
 variable "credentials" {
   type = object({
-    access_key = AKIAWHX3VKYV7B4EFXVL
-    secret_key = 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
+    access_key = "AKIAWHX3VKYV7B4EFXVL"
+    secret_key = "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"
   })
 
   description = "My AWS credentials"
