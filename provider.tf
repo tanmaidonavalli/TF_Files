24699f95d131aa752e176c97ac5231758a9c33dc
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-05 20:23:59 -0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,19 +1,9 @@
 provider "aws"{
-    region = "${var.AWS_REGION}"
-    profile    = "${var.profile}"
+    region = "us-east-1" 
 }
 
-resource "aws_instance" "instance1" {
-  #count = 2
-  ami = "${lookup(var.AMIS, var.AWS_REGION)}"
-  instance_type = "t2.micro"
-  security_groups = ["${aws_security_group.tf-sg.name}"]  
 
-  tags = {
-      #Name = "tinstance ${count.index}"
-      Name = "tinstance"
-  }
-}
+
 
 
 
