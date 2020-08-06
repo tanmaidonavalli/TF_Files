0f83d5c49d01b928cd6a72bc472d0b980c8c9e76
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-05 20:43:46 -0700

    Create vars.tf

---
+++ vars.tf
@@ -0,0 +1,8 @@
+variable "credentials" {
+  type = object({
+    access_key = AKIAWHX3VKYV7B4EFXVL
+    secret_key = 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
+  })
+
+  description = "My AWS credentials"
+}
