b90af78421b993eb60a4eb1b9dceaaa365cac226
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-06 11:47:21 -0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,6 +4,18 @@ provider "aws" {
   secret_key = "${var.secret_key}"
 }
 
+resource "aws_instance" "instance1" {
+  #count = 2
+  ami = "${lookup(var.AMIS, var.AWS_REGION)}"
+  instance_type = "t2.micro"
+  security_groups = ["${aws_security_group.tf-sg.name}"]  
+
+  tags = {
+      #Name = "tinstance ${count.index}"
+      Name = "tinstance"
+  }
+}
+
 
 
 
