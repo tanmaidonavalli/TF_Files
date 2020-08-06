d6d5ed7875f88adb4ebe4f7178df4f6aaed719c6
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-06 14:35:07 -0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,6 @@
 provider "aws" {
     region = "us-east-1" 
-    access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
+    profile    = "${var.profile}"
 }
 
 resource "aws_instance" "instance1" {
