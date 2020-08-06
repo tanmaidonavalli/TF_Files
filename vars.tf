234d2511ddf4090c5a4e17929ccdaf1bcb03cdf1
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-05 20:54:50 -0700

    Delete vars.tf

--- vars.tf
+++
@@ -1,8 +0,0 @@
-variable "credentials" {
-  type = object({
-    access_key = "AKIAWHX3VKYV7B4EFXVL"
-    secret_key = "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"
-  })
-
-  description = "My AWS credentials"
-}
