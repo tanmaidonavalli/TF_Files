202c16feb3fde09fe97661580b521e1274d98284
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-07 09:05:25 -0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,8 +6,7 @@ provider "aws" {
 resource "aws_instance" "instance1" {
   #count = 2
   ami = "${lookup(var.AMIS, var.AWS_REGION)}"
-  instance_type = "t2.micro"
-  security_groups = ["${aws_security_group.tf-sg.name}"]  
+  instance_type = "t2.micro"  
 
   tags = {
       #Name = "tinstance ${count.index}"
