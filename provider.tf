ec10ea62aafbe166b0ad41250c21fa06f90bab68
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-06 19:58:27 -0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-    region = "us-east-1" 
-    profile    = "${var.profile}"
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
-
 resource "aws_instance" "instance1" {
   #count = 2
   ami = "${lookup(var.AMIS, var.AWS_REGION)}"
