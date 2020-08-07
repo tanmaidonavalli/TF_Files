b6d24c74314d58fa51f967fa8760b0680ea8ef83
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-07 08:47:16 -0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = var.aws_region
-  #access_key = var.aws_access_key
-  #secret_key = var.aws_secret_key
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 resource "aws_instance" "instance1" {
   #count = 2
