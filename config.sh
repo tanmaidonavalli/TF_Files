49b4b028dddbb0b2c2581ca812349adbbe5e7d7a
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-10 12:25:16 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-$word= "FAILURE"
-$file= "log.txt"
-exists=$(grep -c $word $file)
-if [[ $exists -gt 0 ]]; then
-    echo "Word found"
-fi
+
+filename="log.txt"
+
+#While loop to read line by line
+while IFS= read -r line; do
+    #If the line starts with ST then set var to yes.
+    if [[ $line == FAILURE ]] ; then
+        printline="yes"
+        # Just t make each line start very clear, remove in use.
+        echo "FAILURE IS IN LOG"
+    fi
