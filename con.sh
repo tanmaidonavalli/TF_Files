0a73b72a8f91325d6cdd1efb08e14f68b13b6d55
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-10 15:51:51 -0700

    Update con.sh

--- con.sh
+++ con.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 read -a FAILURE <  log.txt
-for line in "${FAILURE[@]}"; do
-echo "Failure happening"
- done
+#for line in "${FAILURE[@]}"; do
+#echo "Failure happening"
+ #done
  
  echo "Failure"
  
