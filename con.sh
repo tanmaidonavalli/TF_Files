677840ea84adbb99a0d0b8d3bea10a792b312de3
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-10 16:31:34 -0700

    Update con.sh

--- con.sh
+++ con.sh
@@ -2,9 +2,9 @@ isInFile=$(cat log.txt | grep -c "FAILURE")
 
 
 if [ $isInFile -eq 0 ]; then
-   string not contained in file
+   echo "string not contained in file"
 else
-   string is in file at least once
+   "string is in file at least once"
 fi
  
 
