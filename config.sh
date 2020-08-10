eb4291a821b09573720916528cca68642bed7c14
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-10 14:46:58 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-filename="log.txt"
-
-#While loop to read line by line
-while IFS= read -r line; do
-    #If the line starts with ST then set var to yes.
-    if [[ $line == FAILURE ]] ; then
-        printline="yes"
-        # Just t make each line start very clear, remove in use.
-        echo "FAILURE IS IN LOG"
-    fi
-    
-    done < "$filename"
+read -a FAILURE <  log.txt
+for line in "${FAILURE[@]}"; do
+echo "Failure happening"
+ done
