af7d818bc5250a7cde47ed7e8dfcae7aa1e54baa
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-10 10:42:34 -0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -3,7 +3,7 @@ filename="/root/TFCode/TF_Files/log.txt"
 
 while IFS= read -r line; do
             #If the line starts with ST then set var to yes.
-                if [[ $line == Failure ]] ; then
+                if [[ $line == FAILURE ]] ; then
 
                                         # Just t make each line start very clear, remove in use.
                                                 echo "Failure"
