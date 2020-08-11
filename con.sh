e774106fb6215043401d5940118258759dd20bcd
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-11 10:19:32 -0700

    Update con.sh

--- con.sh
+++ con.sh
@@ -2,74 +2,15 @@
 #!/bin/bash
 config-lint -terraform . >log.txt
 if [[ "$(cat log.txt | grep -c "FAILURE")" -ne 0 ]]; then
-              echo "failure"
+              echo "failure seen in log file exiting"
  exit1
  fi
 
  if [[ "$(cat log.txt | grep -c "WARNING")" -ne 0 ]]; then
-          echo "warning"
+          echo "warning seen in log file. Please check the code"
  else
           echo "wohooo"
   fi
 
 
-
-
-#!/bin/bash
-
-#declare file="log.txt"
-#declare regex="WARN"
-#file_content=$( cat "${file}" )
-
-#if [[ " $file_content " =~ $regex ]]
-#then
- 
- 
-#echo "warning" 
-
-#fi
-
-#exit
-
-
-
-#isInFile=$(cat log.txt | grep -c "FAI*")
-
-
-#if [ $isInFile -eq 0 ]; then
-   #echo "Failure STATUS"
-   #exit 1
-#else
-#fi
-   #if [ $isInFile -eq WAR* ];
-   #echo "Warning message for code"
-#fi
- 
- #if grep -xq "FAILURE" log.txt ; then
-  # code for if it exists 
-  #echo "file exists"
-#else
-  # code for if it does not exist
-#  echo "noooo"
-#fi 
-#!/bin/bash
-#read -a FAILURE <  log.txt
-#for line in "${FAILURE[@]}"; do
-#echo "Failure happening"
- #done
-
-
-#else
-        #echo "checking failure"
-
-#fi
-#exit
-    
-#if [[ " $file_content " =~ "FAILURE" ]] ; 
-#then
-#echo "EXITING"
-
-
-#exit 1
-#fi
  
