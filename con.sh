caf3a2dd1ef04de581d5a8caa5e9ca4bfd7011ec
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-10 17:33:05 -0700

    Update con.sh

--- con.sh
+++ con.sh
@@ -1,13 +1,45 @@
-isInFile=$(cat log.txt | grep -c "FAI*")
+#isInFile=$(cat log.txt | grep -c "FAI*")
 
 
-if [ $isInFile -eq FAI* ]; then
-   echo "Failure STATUS"
-   exit 1
-else
-   if [ $isInFile -eq WAR* ];
-   echo "Warning message for code"
-fi
+#if [ $isInFile -eq 0 ]; then
+   #echo "Failure STATUS"
+   #exit 1
+#else
+#fi
+   #if [ $isInFile -eq WAR* ];
+   #echo "Warning message for code"
+#fi
  
+ #if grep -xq "FAILURE" log.txt ; then
+  # code for if it exists 
+  #echo "file exists"
+#else
+  # code for if it does not exist
+#  echo "noooo"
+#fi  
 
+#!/bin/bash
+
+declare file="log.txt"
+declare regex="WARN"
+file_content=$( cat "${file}" )
+
+if [[ " $file_content " =~ $regex ]]
+then
+ echo "found warning deprecated"
+
+
+#else
+        #echo "checking failure"
+
+#fi
+#exit
+    
+#if [[ " $file_content " =~ "FAILURE" ]] ; 
+#then
+#echo "EXITING"
+
+
+#exit 1
+#fi
  
