6947e4935d31ac957a3464756afe1933e8118667
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-11 12:24:56 -0700

    Update con.sh

--- con.sh
+++ con.sh
@@ -2,14 +2,14 @@
 #!/bin/bash
 config-lint -terraform . >log.txt
 if [[ "$(cat log.txt | grep -c "FAILURE")" -ne 0 ]]; then
-              echo "$(tput setaf 1)Failure seen in log file exiting"
+              echo "$(tput setaf 3)Failure seen in log file exiting"
  exit 1
  fi
 
  if [[ "$(cat log.txt | grep -c "WARNING")" -ne 0 ]]; then
-          echo "$(tput setaf 1)Warning seen in log file. Please check the code"
+          echo "$(tput setaf 3)Warning seen in log file. Please check the code"
  else
-          echo "$(tput setaf 1)Wohooo no errors in config-lint log"
+          echo "$(tput setaf 3)Wohooo no errors in config-lint log"
   
   fi
 
