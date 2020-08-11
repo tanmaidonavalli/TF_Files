1a1bee57fc5126e06b1e396153b21ab2cd92b279
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-11 10:41:08 -0700

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,12 +1,4 @@
-variable "AMIS" {
-    type = "map"
-    default = {
-        us-east-1 = "ami-085925f297f89fce1"
-        us-west-1 = "ami-02c4faee7407cf92e"
-        
-    }
-  
-}
+
 
 variable "aws_access_key" {
   type = string
