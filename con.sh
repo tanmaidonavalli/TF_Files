93cc4ee000bd3d0db7a158163f8d74670302c690
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-17 10:11:19 -0700

    Update con.sh

--- con.sh
+++ con.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
-#terraform workspace new demo
-terraform init 
-config-lint -terraform .
-# | tee -a "log.txt"
-#if [[ "$(cat log.txt | grep -c "FAILURE")" -ne 0 ]]; then
- #             echo "Failure seen in config-lint log file"
- #exit 1
- #fi
 
- #if [[ "$(cat log.txt | grep -c "WARNING")" -ne 0 ]]; then
-  #        echo "Warning seen in log file. Please check the code"
- #else
-  #        echo "Wohooo no errors in config-lint log"
-  
-  #fi
+terraform init 
+ config-lint -terraform .
  terraform plan 
  terraform apply 
 
