f774b5107ce24bc01b0d5779079aa4ad73a39600
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-17 09:43:52 -0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,7 +7,7 @@ provider "aws" {
 resource "aws_instance" "instance1" {
   #count = 2
   ami = "${lookup(var.AMIS, var.AWS_REGION)}"
-  instance_type = "t2.micro"
+  instance_type = "db2.micro"
     
 
   tags = {
