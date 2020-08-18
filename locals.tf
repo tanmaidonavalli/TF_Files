6f8d124149106ae4758a6737ece18fc31f6dec7c
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-17 18:56:54 -0700

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,15 +1,11 @@
 locals {
   env = {
-    labs-us-east-1 = {
-      name                    = "labs"
-      vpc_cidr                = "10.1.0.0/16"
-      multi_nat               = false
+    app1-dev {
+      name                    = "dev"
     }
 
-    prod-us-east-1 = {
+    app1-prod = {
       name                    = "prod"
-      vpc_cidr                = "10.0.0.0/16"
-      multi_nat               = true
     }
   }
 
