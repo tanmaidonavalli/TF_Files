e9d24f93957678c2dfc51a947004269d4ebfb0b8
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-17 19:00:52 -0700

    Update con.sh

--- con.sh
+++ con.sh
@@ -3,19 +3,19 @@
  set -e
  terraform init 
  config-lint -terraform .
- terraform workspace new "${ENV}" 
+ terraform workspace new "app1-dev
  terraform init
  terraform validate
 
 if [[ ${EXEC} == "plan" ]]
 then
-terraform plan -out="${ENV}.tfplan"
+terraform plan -out="app1-dev.tfplan
 fi
 
 if [[ ${EXEC} == "apply" ]]
 then
-terraform apply "${ENV}.tfplan"
-rm -f "${ENV}.tfplan"
+terraform apply "app1-dev.tfplan"
+rm -f "app1-dev.tfplan"
 fi
 
 
