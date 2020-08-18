a5f8f6dcca0c8000e1fc19cb16d1aaabd790aab3
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-18 12:58:18 -0700

    Delete backend.tf

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    key            = "terraform.tfstate"
-    bucket         = "app1"
-    dynamodb_table = "terraform-state-lock-app1dev"
-    region         = "us-east-1"
-    encrypt        = true
-    }
-}
