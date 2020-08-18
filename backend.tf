7b131f40b1426f75bbb74fc4709387fdd7e043ee
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-18 11:11:05 -0700

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "app1-dev-12795"
     key            = "terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-state-lock-app1dev"
+    #dynamodb_table = "terraform-state-lock-app1dev"
     encrypt        = true
   }
 }
