8e344cc4431f887477e4e5f18b294d6c74ce6cb6
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-18 11:28:29 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,14 +15,4 @@ resource "aws_s3_bucket" "tf_code" {
   }
 }
 
-resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
- name = "${var.project_name}"
-  hash_key = "LockID"
-  read_capacity = 20
-  write_capacity = 20
-attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
 
