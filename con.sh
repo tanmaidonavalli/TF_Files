3d234210f0ddcc6740a73e1407a2020f470cb6f3
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-17 18:03:18 -0700

    Update con.sh

--- con.sh
+++ con.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
-
-terraform init 
+ set -x
+ set -e
+ ENV="${1}"
+ EXEC="${2}"
+ terraform init 
  config-lint -terraform .
+ terraform workspace select "${ENV}" || (terraform workspace new "${ENV}" && terraform workspace select "${ENV}")
+ terraform init
+ terraform validate
+
+if [[ ${EXEC} == "plan" ]]
+then
+terraform plan -var-file="${ENV}.tfvars" -out="${ENV}.tfplan"
+fi
+
+if [[ ${EXEC} == "apply" ]]
+then
+terraform apply "${ENV}.tfplan"
+rm -f "${ENV}.tfplan"
+fi
+
+
  terraform plan 
  terraform apply 
 
