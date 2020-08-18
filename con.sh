a447691a11fbf7123f9c09305a041806f8ec15f1
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-17 19:11:16 -0700

    Update con.sh

--- con.sh
+++ con.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
  
- terraform init
- terraform workspace new "app1-dev"
+ #terraform init
+ #terraform workspace new "app1-dev"
  
- config-lint -terraform .
- terraform validate
- terraform plan -out="app1-dev.tfplan
- terraform apply "app1-dev.tfplan"
+ #config-lint -terraform .
+ #terraform validate
+ #terraform plan -out="app1-dev.tfplan
+ #terraform apply "app1-dev.tfplan"
 
 
 
