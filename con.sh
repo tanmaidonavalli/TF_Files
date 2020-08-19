5d0b229fcf36a8185669a1057238d4494c81abf8
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-19 09:33:53 -0700

    Update con.sh

--- con.sh
+++ con.sh
@@ -8,6 +8,7 @@
  #terraform plan -out="app1-dev.tfplan
  #terraform apply "app1-dev.tfplan"
 
+terraform workspace select "app1-dev" || (terraform workspace new "app1-dev" && terraform workspace select "app1-dev") 
 
 
 
