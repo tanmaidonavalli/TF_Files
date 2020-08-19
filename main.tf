9b598b195ab58d5edaa486dd2682e14886dc2f9b
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-18 20:44:22 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@
 resource "aws_s3_bucket" "bucket" {
   bucket = local.name_prefix
   acl    = "private"
-  region = "${lookup(local.region, local.environment)}"
   force_destroy = true
   versioning {
     enabled = true
