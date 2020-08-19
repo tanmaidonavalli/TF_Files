9b617791cd788a82315e3d749b722a43bc08eeb2
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-19 12:48:28 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,10 @@
 
 resource "aws_s3_bucket" "bucketdemo" {
-  bucket = "bucketdemo"
+  bucket = local.name_prefix
+  acl    = "private"
   force_destroy = true
-  
+  versioning {
+    enabled = true
+  }
   tags = local.common_tags
 }
