0ac3637bf93f0cf4d1bf7e5e26ae50746c118d18
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-19 13:14:19 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,7 +13,7 @@ locals {
     Terraform   = "true"
     Environment = local.environment
   }
-  name_prefix = "${local.app_name}"
+  name_prefix = "${local.app_name}-${local.environment}"
 }
 
 variable "aws_access_key" {
