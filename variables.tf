8d9691b78fc7fac23892e3fc4fdad2634359c6da
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-19 10:01:04 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -24,3 +24,18 @@ variable "aws_secret_key" {
   description = "AWS secret key"
 }
 
+variable "AMIS" {
+    type = "map"
+    default = {
+        us-east-1 = "ami-085925f297f89fce1"
+        us-west-1 = "ami-02c4faee7407cf92e"
+        
+    }
+  
+}
+
+
+variable "aws_region" {
+    default = "us-east-1"
+}
+
