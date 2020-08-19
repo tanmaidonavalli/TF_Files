81f86daacf09d2cae4ddbff190aac821eaff71aa
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-19 12:34:14 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 
 resource "aws_s3_bucket" "bucketdemo" {
   bucket = "bucketdemo"
-  region = "us-east-1"
+  region = "${lookup(local.region, local.environment)}"
   force_destroy = true
   
   tags = local.common_tags
