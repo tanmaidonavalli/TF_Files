b2e573ab5d447cefb1ec0b860ecb8aba550b1462
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-19 12:37:15 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@ locals {
   app_name       = "application1"
   environment = "${terraform.workspace}"
   region = {
-    type = map(string)
+    
   "app1-dev" = "us-east-1"
     "app1-prepod" = "us-east-1"
 }
