e10c6f36d41fed03241bdb9bb838c1d4959e6655
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-19 12:16:38 -0700

    Update con.sh

--- con.sh
+++ con.sh
@@ -7,6 +7,8 @@
  #terraform validate
  #terraform plan -out="app1-dev.tfplan
  #terraform apply "app1-dev.tfplan"
+ 
+ env='app1-dev'
 terraform workspace list 
 terraform workspace select "${ENV}"  || (terraform workspace new "${ENV}" && terraform workspace select "${ENV}")
 
