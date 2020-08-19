292af2a4bd7fcea3890f54c5db9d57df51ef06eb
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-19 12:25:39 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,4 @@
 
-resource "aws_s3_bucket" "bucket" {
-  bucket = local.name_prefix
-  acl    = "private"
-  region = "us-east-1"
-  force_destroy = true
-  versioning {
-    enabled = true
-  }
-  tags = local.common_tags
-}
-
 resource "aws_s3_bucket" "bucketdemo" {
   bucket = "bucketdemo"
   region = "us-west-1"
