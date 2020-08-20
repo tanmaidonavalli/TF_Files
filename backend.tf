b2474862655c1454d2615cd6f6c908bec82da3de
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-19 20:22:08 -0700

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,4 +1,12 @@
 
 terraform {
-  
+  required_version = "~> 0.12"
+  backend "s3" {
+    key            = "terraform.tfstate"
+    bucket         = "bucketforworkspacestate"
+    dynamodb_table = "terraform-state-lock-app1dev"
+    region = "us-east-1"
+    encrypt        = true
+    workspaces { prefix = "workspaces" }
+  }
 }
