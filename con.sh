a383ab5647d9a38bcf23bd962a6b4c921611f5d1
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-20 10:52:47 -0700

    Update con.sh

--- con.sh
+++ con.sh
@@ -1,6 +1,19 @@
 #!/bin/bash 
 ENV="app1-dev"
-config-lint -terraform .
+
+config-lint -terraform . | tee -a "log.txt"
+if [[ "$(cat log.txt | grep -c "FAILURE")" -ne 0 ]]; then
+              echo "Failure seen in log file exiting"
+ exit 1
+ fi
+
+ if [[ "$(cat log.txt | grep -c "WARNING")" -ne 0 ]]; then
+          echo "Warning seen in log file. Please check the code"
+ else
+          echo "Wohooo no errors in config-lint log"
+  
+  fi
+  
 terraform workspace list 
 terraform workspace select $ENV  || (terraform workspace new $ENV && terraform workspace select $ENV)
 workspace=$(terraform workspace show)
@@ -15,22 +28,3 @@ echo "current workspace is $workspace"
 
 
 
-#config-lint -terraform .
-
-
-
-
-#terraform init
- #terraform workspace new "app1-prepod"
- 
- 
- #terraform validate
- #terraform plan -out="app1-dev.tfplan
- #terraform apply "app1-dev.tfplan"
-
-
-                                
-
-
-
- 
