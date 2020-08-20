8f8c16c2e1417283900c83573254b6b0c37ca5ca
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-19 20:10:05 -0700

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_version = "~> 0.12"
+  backend "s3" {
+    key            = "terraform.tfstate"
+    bucket         = "samplebucketfordemo"
+    dynamodb_table = "terraform-state-lock-app1dev"
+    region = "us-east-1"
+    encrypt        = true
+    workspaces { prefix = "workspaces" }
+  }
+}
