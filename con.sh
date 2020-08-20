05bad28556ca3e50eea21a686669dfd17fa4252d
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-19 20:01:06 -0700

    Update con.sh

--- con.sh
+++ con.sh
@@ -1,24 +1,28 @@
-#!/bin/bash
+#!/bin/bash 
+ENV="app1-dev"
+ENV="app1-prod"
+
+declare -a StringArray=("app1-dev","app1-prod","app1-staging")
  
- #terraform init
+# Iterate the string array using for loop
+for val in ${StringArray[@]}; do
+terraform workspace list 
+terraform workspace select $ENV  || (terraform workspace new $ENV && terraform workspace select $ENV)
+workspace=$(terraform workspace show)
+echo "current workspace is $workspace"
+done
+
+
+
+
+
+#terraform init
  #terraform workspace new "app1-prepod"
  
  #config-lint -terraform .
  #terraform validate
  #terraform plan -out="app1-dev.tfplan
  #terraform apply "app1-dev.tfplan"
- 
-ENV="app1-dev"
-ENV="app1-prod"
-
-
-terraform workspace list 
-terraform workspace select $ENV  || (terraform workspace new $ENV && terraform workspace select $ENV)
-workspace=$(terraform workspace show)
-echo "current workspace is $workspace"
-
-
-
 
 
                                 
