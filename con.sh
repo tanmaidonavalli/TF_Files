454941067b49f1b3b5c59e5eb0783386387b7375
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-19 19:37:48 -0700

    Update con.sh

--- con.sh
+++ con.sh
@@ -8,19 +8,14 @@
  #terraform plan -out="app1-dev.tfplan
  #terraform apply "app1-dev.tfplan"
  
-ENV="preprod"
-#ENV=(app1-prod,app1-dev,app1-uat,app1-stage)
+#ENV="preprod"
+ENV=(app1-prod,app1-dev,app1-uat,app1-stage)
 
 terraform workspace list 
 terraform workspace select $ENV  || (terraform workspace new $ENV && terraform workspace select $ENV)
 workspace=$(terraform workspace show)
 echo "current workspace is $workspace"
-tfvars_file="$workspace.tfvars"
-if test -f $tfvars_file; then
-echo "found $tfvard_file, applying"
-terraform apply -var-file=$workspace.tfvars
-else echo "cannot find $tfvars_file"
-fi
+
 
 
 
