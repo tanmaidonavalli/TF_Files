037d6be6facdfe61950635935660e373955027d2
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-08-20 10:43:18 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,5 +6,12 @@ resource "aws_s3_bucket" "workspacesdemo" {
   versioning {
     enabled = true
   }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
   tags = local.common_tags
 }
