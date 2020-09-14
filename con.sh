924edd2af5452fc09548ae0353b73cd3ec54e60c
Tanmai Donavalli <tanmaidonavalli20@gmail.com>
2020-09-14 07:01:34 -0700

    Update con.sh

--- con.sh
+++ con.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 #Use environment variables when integrating with CI/CD 
-ENV="app2-dev"
+ENV="app2-test"
 
 terraform workspace list 
 terraform workspace select $ENV  || (terraform workspace new $ENV && terraform workspace select $ENV)
